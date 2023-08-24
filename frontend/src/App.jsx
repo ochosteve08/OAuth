4ef910a3572be6f0ee553218282444c950ed0c3a
ochosteve <ochosteve08@gmail.com>
2023-08-24 23:55:38 +0100

    install router package and create pages routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    </div>
+    <Router>
+     
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
