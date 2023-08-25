4cf644e7420aa7826fba3df45c85d01ba639bb5e
ochosteve <ochosteve08@gmail.com>
2023-08-25 15:50:19 +0100

    add encryption to password

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,14 +1,16 @@
-import { json } from "express"
-import UserModel from "../model/user.model.js"
+import { json } from "express";
+import UserModel from "../model/user.model.js";
+import bcryptjs from "bcryptjs";
 
-export const Signup = async (req, res)=>{
-   try{ console.log(req.body)
-    const {username, email, password} = req.body
-   const newUser = await  UserModel.create({username, email, password})
-  res.json(newUser)
-}
-catch (err) {
-    console.log({message: err.message})
-    res.status(500).json({message: err.message})
-}
-}
+export const Signup = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { username, email, password } = req.body;
+    const hashPassword = bcryptjs.hashSync(password, 10);
+    const newUser = await UserModel.create({ username, email, password: hashPassword });
+    res.json(newUser);
+  } catch (err) {
+    console.log({ message: err.message });
+    res.status(500).json({ message: err.message });
+  }
+};
