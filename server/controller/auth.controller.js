e4375ad433c7b88b8204d8bbf7b90e00fa5343ed
ochosteve <ochosteve08@gmail.com>
2023-08-25 16:55:47 +0100

    add error handling

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -2,15 +2,15 @@ import { json } from "express";
 import UserModel from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const Signup = async (req, res) => {
+export const Signup = async (req, res, next) => {
   try {
-    console.log(req.body);
+   
     const { username, email, password } = req.body;
     const hashPassword = bcryptjs.hashSync(password, 10);
     const newUser = await UserModel.create({ username, email, password: hashPassword });
     res.json(newUser);
-  } catch (err) {
-    console.log({ message: err.message });
-    res.status(500).json({ message: err.message });
+  } catch (error) {
+     
+    next(error);
   }
 };
