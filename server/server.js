9a39229f5a3d7f8d5d3bd9d4cf80a44facd7af6c
ochosteve <ochosteve08@gmail.com>
2023-08-25 22:04:20 +0100

    complete signup page functionality

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./route/user.route.js";
 import authRoute from "./route/auth.route.js";
+import cors from 'cors';
+
 
 dotenv.config();
 
@@ -10,6 +12,14 @@ const app = express();
 const port = 3500;
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
