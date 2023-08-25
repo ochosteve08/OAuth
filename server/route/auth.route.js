8c7e2f7bb7ef6aedd7c982dfa38a754b09de4412
ochosteve <ochosteve08@gmail.com>
2023-08-25 15:40:29 +0100

    add signup controller logic

---
+++ server/route/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { Signup } from "../controller/auth.controller.js";
+
+const router = express.Router();
+
+router.post('/signup', Signup)
+
+
+export default router
