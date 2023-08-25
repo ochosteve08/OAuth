b4b5f8b156e34b8969405fc6e44baec6aee94bbe
ochosteve <ochosteve08@gmail.com>
2023-08-25 14:45:39 +0100

    create controller directory

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,10 +1,8 @@
-import express from 'express';
+import express from "express";
+import { CreateUser } from "../controller/user.controller.js";
 
 const router = express.Router();
 
-
-router.get("/", (req, res) => {
-  res.json({ message: "fetch users" });
-});
+router.get("/",CreateUser);
 
 export default router;
