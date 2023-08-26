601b296c4418969569da09aa4c516f021979bdbb
ochosteve <ochosteve08@gmail.com>
2023-08-27 00:25:50 +0100

    complete google auth at client side

--- frontend/src/Components/Oauth.jsx
+++ frontend/src/Components/Oauth.jsx
@@ -1,13 +1,32 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../Firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../features/user/UserSlice";
 
 const Oauth = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const googleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const result = await signInWithPopup(auth, provider);
-      console.log(result);
-    //   const credential = GoogleAuthProvider.credentialFromResult(result);
+      const response = await fetch("http://localhost:3500/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
     } catch (error) {
       console.log("could not login with google", error);
     }
