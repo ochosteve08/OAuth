12e4645c265bc4bf4ce44e130ac7ff6508d4ab36
ochosteve <ochosteve08@gmail.com>
2023-08-26 20:07:00 +0100

    create userSlice for user login

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -1,7 +1,12 @@
+
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from '../features/user/UserSlice'
+
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
   middleware:(getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck:false,
   })
--- frontend/src/features/user/UserSlice.jsx
+++ frontend/src/features/user/UserSlice.jsx
@@ -1,9 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  currentUser: null,
+  loading: false,
+  error: false,
+  success: false,
+  errMsg: "",
+};
 export const userSlice = createSlice({
   name: "user",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.success = false;
+      state.error = false;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+      state.success = true;
+    },
+    signInFailure: (state, action) => {
+      state.errMsg = action.payload;
+      console.log(action.payload);
+      //   console.log(action.payload.message);
+      state.loading = false;
+      state.success = false;
+      state.error = true;
+    },
+  },
 });
+
+export const { signInFailure, signInStart, signInSuccess } = userSlice.actions;
+
+export const showLoading = (state) => state.user.loading;
+export const showError = (state) => state.user.error;
+export const showErrorMessage = (state) => state.user.errMsg;
+export const showSuccess = (state) => state.user.success;
+export const userDetails = (state) => state.user.currentUser;
 
 export default userSlice.reducer;
