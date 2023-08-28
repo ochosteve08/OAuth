a22e695b89dcc2632f73a59c8a37c337faa8e0ec
ochosteve <ochosteve08@gmail.com>
2023-08-28 11:53:58 +0100

    add delete and signout routes

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from "../controller/user.controller.js";
+import { test, updateUser, deleteUser } from "../controller/user.controller.js";
 import { verifyToken } from "../middleware/verify.user.js";
 
 const router = express.Router();
 
-router.get("/",test);
-router.patch("/:id",verifyToken, updateUser);
+router.get("/", test);
+router.patch("/:id", verifyToken, updateUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
